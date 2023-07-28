c2c97b903cd0e87f5ee3a8c58eb2c6c52d61eef2
Jose Deodoro <deodoro.filho@gmail.com>
2023-07-28 11:32:12 -0500

    Build script

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
-rm -rf docker/*.py docker/requirements.txt docker/dist
-cp chat-server-py/*.py docker
-cp chat-server-py/requirements.txt docker
-API_URL=/chat/api npm run build --prefix client
-mv client/dist docker/
-cd docker
+echo "Cleaning up"
+rm -rf build/*.py build/requirements.txt build/dist
+echo "Copying server files"
+cp server/*.py server/requirements.txt build
+echo "Building client"
+cd client
+docker build -t react-builder .
+echo "Deploying client"
+cd ../build
+container=$(docker create react-builder)
+docker cp $container:/app/dist ./
+echo "Building server"
 docker build -t q_and_a .
+echo "Done"
+cd ..
